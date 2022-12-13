da42cf423f03b30c5b2d7a4e861910de9fd01963
flesarradu <flesarradu@gmail.com>
2022-12-13 02:17:33 +0200

    Add project files.

---
+++ Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeHttpCalls_UTCN
+{
+    public class Message
+    {
+        public long Id { get; set; }
+        public string User { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Http.Json;
+using FakeHttpCalls_UTCN;
+
+int id = 4;
+            foreach (var line in System.IO.File.ReadAllLines(@"C:\Users\flesa\OneDrive\Documents\paul_bolnav.txt"))
+            {
+                using (var client = new HttpClient())
+                {
+                    Message mess = new Message { Id = id, User = "paul", Text = line };
+                
+                    client.BaseAddress = new Uri("https://rest-apiproiect2utcn20221212231729.azurewebsites.net");
+                    var response = client.PostAsJsonAsync("api/Messages", mess).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Bravoo");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nu bravoo");
+                    }
+                    id++;
+                }
+            }
